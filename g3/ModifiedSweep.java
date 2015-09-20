e56152967212976c67f2112fe3c6dfc0f7600e8e
Dhruv Purushottam <dhruvpurushottam@dyn-160-39-132-148.dyn.columbia.edu>
2015-09-19 22:10:43 -0400

    Picks random closest rat

--- g3/ModifiedSweep.java
+++ g3/ModifiedSweep.java
@@ -79,7 +79,10 @@ public class ModifiedSweep implements pppp.g3.Strategy {
 						}
 					} else {
 						if (pos_index[p] == 1) {
-							dst = findClosest(pipers[id][p], rats, 1);
+							int n_closest = 3;
+							int random_closest = Math.round(n_closest * Math.random());
+							Point[] cloest_points = findClosest(pipers[id][p], rats, n_closest);
+							dst = cloest_points[n];
 							pos[p][pos_index[p]] = dst;
 						} else if (pos_index[p] == 2) {
 							dst = returnToSender(pipers, p);
